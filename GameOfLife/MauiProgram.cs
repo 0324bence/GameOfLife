c5e6adde77d476b79ce1d398655386819ddbfbe0
timre13 <itorteli13@gmail.com>
2023-01-20 12:12:48 +0100

    Remove font loading

--- GameOfLife/MauiProgram.cs
+++ GameOfLife/MauiProgram.cs
@@ -5,13 +5,7 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+		builder.UseMauiApp<App>();
 
 		return builder.Build();
 	}
