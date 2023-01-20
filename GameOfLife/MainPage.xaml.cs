b449942327bb1a73b990e18890833ad51197a639
timre13 <itorteli13@gmail.com>
2023-01-20 12:12:39 +0100

    Highlight hovered cell

--- GameOfLife/MainPage.xaml.cs
+++ GameOfLife/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace GameOfLife;
 
@@ -12,38 +17,86 @@ public partial class MainPage : ContentPage
         drawable.CanvasSizePX = (int)view.WidthRequest;
         view.Drawable = drawable;
 		view.StartInteraction += drawable.OnClick;
-        //view.MoveHoverInteraction += drawable.OnClick;
-	}
+        view.MoveHoverInteraction += drawable.OnHoverMove;
+        view.EndHoverInteraction += drawable.OnHoverEnd;
+    }
+}
+
+struct Pos
+{
+    public int Col;
+    public int Row;
+
+    public Pos(int col, int row)
+    {
+        Col = col;
+        Row = row;
+    }
 }
 
 public class Drawable : IDrawable
 {
-	public static readonly int s_gridSpacing = 10;
-	public int CanvasSizePX = 0;
+    public static readonly int GRID_SIZE = 50;
+
+    private int _canvasSizePx = 0;
+    public int _gridSpacing = 0;
+    public int CanvasSizePX
+    {
+        get => _canvasSizePx;
+        set {
+            _canvasSizePx = value;
+            _gridSpacing = _canvasSizePx / GRID_SIZE;
+        }
+    }
+
+    private Pos _previewPos = new Pos(-1, -1);
 
 	public void OnClick(object sender, TouchEventArgs e)
-	{
-		//points.Add(e.Touches.First());
-		var view = (sender as GraphicsView);
-		view.Invalidate();
-        Debug.WriteLine($"{e.Touches.First().X}, {e.Touches.First().Y}");
-	}
+    {
+        Debug.WriteLine($"Click: {e.Touches.First().X}, {e.Touches.First().Y}");
+
+        var view = (sender as GraphicsView);
+        view.Invalidate();
+    }
+
+    public void OnHoverMove(object sender, TouchEventArgs args)
+    {
+        Debug.WriteLine($"Hover: {args.Touches.First().X}, {args.Touches.First().Y}");
+
+        _previewPos.Col = (int)((args.Touches.First().X) / _gridSpacing);
+        _previewPos.Row = (int)((args.Touches.First().Y) / _gridSpacing);
+
+        var view = (sender as GraphicsView);
+        view.Invalidate();
+    }
+    public void OnHoverEnd(object sender, EventArgs args)
+    {
+        Debug.WriteLine($"Hover end");
+
+        _previewPos = new Pos(-1, -1);
+
+        var view = (sender as GraphicsView);
+        view.Invalidate();
+    }
+
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
-        canvas.FillColor = Colors.Tomato;
-        canvas.FillRectangle(s_gridSpacing, s_gridSpacing, s_gridSpacing, s_gridSpacing);
-
+        canvas.StrokeSize = 1;
         canvas.StrokeColor = Colors.Black;
 
-        for (int i = 0; i <= CanvasSizePX; i += s_gridSpacing)
+        for (int i = 0; i <= CanvasSizePX; i += _gridSpacing)
         {
             canvas.DrawLine(i, 0, i, CanvasSizePX);
         }
 
-        for (int i = 0; i <= CanvasSizePX; i += s_gridSpacing)
+        for (int i = 0; i <= CanvasSizePX; i += _gridSpacing)
         {
             canvas.DrawLine(0, i, CanvasSizePX, i);
         }
+
+        canvas.StrokeSize = 3;
+        canvas.StrokeColor = Colors.Salmon;
+        canvas.DrawRectangle(_previewPos.Col * _gridSpacing, _previewPos.Row * _gridSpacing, _gridSpacing, _gridSpacing);
     }
 }
 
