c2b27435d15fd50a7f9206b754cc0b4708afbd85
DeliBence <DeliBence@13-15>
2023-01-20 08:47:23 +0100

    Add project files.

---
+++ GameOfLife/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace GameOfLife;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(850, 850));
+            var presenter = appWindow.Presenter as OverlappedPresenter;
+            presenter.IsResizable = false;
+        });
+
+                MainPage = new MainPage();
+	}
+}
---
+++ GameOfLife/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace GameOfLife;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		var drawable = new GraphicsDrawable();
+		graphics.Drawable = drawable;
+		graphics.StartInteraction += drawable.OnInteract;
+	}
+
+	
+}
+
+public class GraphicsDrawable : IDrawable
+{
+	private List<PointF> points = new List<PointF>();
+
+	public void OnInteract(object sender, TouchEventArgs e)
+	{
+		points.Add(e.Touches.First());
+		(sender as GraphicsView).Invalidate();
+	}
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+		canvas.FillColor = Colors.Blue;
+        foreach (var p in points)
+		{
+			canvas.FillCircle(p, 4f);
+		}
+    }
+}
+
